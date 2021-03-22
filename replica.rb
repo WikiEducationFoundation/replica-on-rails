55ff89fcbe20908c4364dfdb7fa9e2e10ec39bd3
Sage Ross <ragesoss@gmail.com>
2021-03-22 13:32:30 -0700

    Start adding support for realistic queries
    This is approaching the queries done in PHP from wikiedudashboard.toolforge.org
    
    It currently has encoding issues that I haven't solved yet.

--- replica.rb
+++ replica.rb
@@ -11,7 +11,7 @@ class Replica
     ActiveRecord::Base.establish_connection(
       adapter: 'mysql2',
       database: "#{database}_p",
-      encoding: 'utf8mb4',
+      encoding: 'utf8',
       host: "#{database}.analytics.db.svc.wikimedia.cloud",
       username: USERNAME,
       password: PASSWORD
@@ -26,6 +26,22 @@ end
 class Page < ActiveRecord::Base
   self.table_name = 'page'
   self.primary_key = 'page_id'
+
+  has_many :revisions, foreign_key: 'rev_page'
 end
 
+class Actor < ActiveRecord::Base
+  self.table_name = 'actor'
+  self.primary_key = 'actor_id'
+
+  has_many :revisions, foreign_key: 'rev_actor'
+end
+
+class Revision < ActiveRecord::Base
+  self.table_name = 'revision_userindex'
+  self.primary_key = 'rev_id'
+
+  belongs_to :page, foreign_key: 'rev_page'
+  belongs_to :actor, foreign_key: 'rev_actor'
+end
 
--- server.rb
+++ server.rb
@@ -6,11 +6,32 @@ get '/' do
 end
 
 before do
-  content_type 'application/json'
+  content_type 'application/json', 'charset' => 'utf-8'
 end
 
 after do
   Replica.close_connection
+end
+
+get '/:database/:username/:start/:end/revisions.json' do
+  Replica.connect(database: params['database'])
+  result = Actor.find_by(actor_name: params['username'])
+                .revisions.joins(:page)
+                .where(rev_timestamp: params['start']..params['end'])
+                .pluck(:page_title, :page_namespace, :rev_page, :rev_timestamp)
+  result.to_json
+end
+
+post '/:database/revisions.json' do
+  puts params
+  Replica.connect(database: params['database'])
+  post_params = JSON.parse(request.body.read)
+  puts post_params
+  result = Revision.where(actor: Actor.where(actor_name: post_params['usernames']))
+                   .where(rev_timestamp: post_params['start']..post_params['end'])
+                   .joins(:page, :actor)
+                   .pluck(:page_title, :page_namespace, :actor_name, :rev_page, :rev_timestamp)
+  result.to_json
 end
 
 get '/:database/last_article.json' do
